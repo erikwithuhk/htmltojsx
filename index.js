57bc707c19f48b2a4349e02caa2529d2d5a49dd4
erikwithuhk <efjonsson@gmail.com>
2018-04-16 13:03:10 -0400

    move index back to lib for transpilation

--- index.js
+++
@@ -1,5 +0,0 @@
-import HTMLtoJSX from './lib/HTMLtoJSX';
-import parseStyles from './lib/parseStyles';
-
-export default HTMLtoJSX;
-export { parseStyles };
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import HTMLtoJSX from './HTMLtoJSX';
+import parseStyles from './parseStyles';
+
+export default HTMLtoJSX;
+export { parseStyles };
